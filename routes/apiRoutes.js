b439e0418c93551709e498438f0ee7e3641f5578
MagicMayo <mike.mayoii@gmail.com>
2019-08-26 09:10:36 -0700

    added to post routes for user, pw, and character creation

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,15 +3,17 @@ const path = require('path');
 
 module.exports = (app) => {
     app.post('/new/user', (req,res)=>{
-        db.User.create()
+        db.User.create(req.body.user);
     })
 
     app.post('/new/word', (req,res)=>{
-        db.Word.create()
+        bcrypt.hash(req.body.pass, 8, (err, hash)=>{
+            db.Word.create(hash);
+        }).catch(err)
     })
 
     app.post('/new/char/:user', (req, res)=>{
         const user = req.params.user;
-        db.Character.create()
+        db.Character.create(req.body)
     })
 }
