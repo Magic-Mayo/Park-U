e82a34ffcb09ef408393710eca255a07b21e5cf7
Magic-Mayo <mike.mayoii@gmail.com>
2019-08-29 02:57:50 -0700

    finished handleAtk function so it gets stats from db if it hasn't done it already on the current fight. continued attack function to generate attacks based on accuracy, defense, and attack strength. setup global variables to be filled with data from db

--- public/js/game.js
+++ public/js/game.js
@@ -1,56 +1,78 @@
 $(document).ready(()=>{
+    let gotUserStats = false;
+    let gotCompStats = false;
     let userHP;
     let userDef;
-    let userAtk;
+    let userAtk = [];
     let userLuck;
     let userAcc;
+    let compHP = 100;
+    console.log(userHP)
+    const id = $('.userId').val().trim();
     
-    const url = window.location.search;
-    
-    
-    
-    const handleAtk = (id) => {
-        let getStats;
-        if (url.indexOf('?userId=') !== -1){
-            getStats = url.split('=')[1];
-        }
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const atk = $('.atk-rng').val().trim();
+        userAtk = atk.split('-');
+        handleAtk(id)
+    }
 
-        console.log(getStats)
-        const queryURL = '/user/stats/' + getStats
-        $.get(queryURL, (stats)=>{
-            // userHp = stats.dataValues.hp;
-            // userDef = stats.dataValues.defense;
-            // userLuck = stats.dataValues.luck;
-            console.log(stats)
-        
-            // if (/* attack button clicked*/){
-                // attack(userAtk, userAcc, 5, userLuck, 100)
-            // } else {
-            //     attack(compAtk, compAcc, userDef, compLuck, userHP)
-            // }
-        })
+    const handleAtk = (id) => {
+        if (!gotUserStats){
+            $.get(`user/stats/${id}`, (stats)=>{
+                if (userHP === undefined && userDef === undefined && userLuck === undefined){
+                    userHP = stats.hp;
+                    userDef = stats.defense;
+                    userLuck = stats.luck;
+                }
+                console.log(stats)
+                gotStats = true;
+            })
+        }
+        attack(userAtk, 90, 5, userLuck, compHP)
     }
 
     const attack = (attack, acc, defense, luck, hp) => {
-        const atk = Math.floor(Math.random() * (100 - 0 + 1));
+        console.log(userHP)
+        let finalAtk;
+        const atkRng = Math.floor(Math.random()*5)
+        const atk = Math.ceil(Math.random() * 100);
         let total = hp + defense;
-        const dblDmg = Math.floor(Math.random()*100);
+        const dblDmg = Math.ceil(Math.random()*100);
         if (atk <= acc){
             if (luck===dblDmg){
-                attack=attack*2;
-                total-=attack;
+                finalAtk = attack[4]*2;
+                total -= finalAtk;
+                console.log('dbl')
             } else {
-                total-=attack;
+                console.log('normal')
+                switch (atkRng){
+                    case 0:
+                        finalAtk = parseInt(attack[0]); break;
+                    case 1:
+                        finalAtk = parseInt(attack[1]); break;
+                    case 2:
+                        finalAtk = parseInt(attack[2]); break;
+                    case 3:
+                        finalAtk = parseInt(attack[3]); break;
+                    case 4:
+                        finalAtk = parseInt(attack[4]); break;
+                }
+                total-=finalAtk;
+                compHP=total;
             }
 
             if(attack===userAtk){
-                compHP=total;
             } else {
                 userHP=total;
             }
+            console.log(`Attack power: ${finalAtk}`, `\n`, `Attack Range: ${atkRng}`, `\n`,  `Attack accuracy base: ${atk}`, `\n`,  `Accuracy: ${acc}`, `\n`,  `HP after defense: ${total}`, `\n`,  `Dbl dmg: ${dblDmg}`)
         } else {
+            console.log(`Attack power: ${finalAtk}`, `\n`, `Attack Range: ${atkRng}`, `\n`,  `Attack accuracy base: ${atk}`, `\n`,  `Accuracy: ${acc}`, `\n`,  `HP after defense: ${total}`, `\n`,  `Dbl dmg: ${dblDmg}`)
+            console.log('missed')
             // send to dialog attack was missed
         }
     }
-    $('#attack').on('submit', handleAtk)
+
+    $('.atk').on('submit', handleSubmit)
 })
