74165e3ac478ac001e1229facc48449cc094ce80
MagicMayo <mike.mayoii@gmail.com>
2019-08-29 19:26:49 -0700

    commented out route for getting user info

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,7 +4,6 @@ const bcrypt = require('bcrypt')
 
 module.exports = (app) => {
     app.post('/new/user', (req,res)=>{
-        console.log(req.body)
         bcrypt.hash(req.body.newWord, 8).then((hash)=>{
             db.User.create({userName: req.body.newUser, pass: hash}).then(created=>{
                 if (created){
@@ -24,14 +23,14 @@ module.exports = (app) => {
     app.post('/word/verify', (req,res)=>{
         console.log(req.body)
         db.User.findOne({where: {userName: req.body.user.toString()}}).then(pass=>{
-            console.log(pass.dataValues.pass)
             bcrypt.compare(req.body.userPass, pass.dataValues.pass).then((result)=>{
                 if(result){
-                    res.json(result)
+                    res.redirect('/start')
                 } else {
                     res.json(false)
                 }
-            })
+            });
+            
         })
     })
 
@@ -42,9 +41,12 @@ module.exports = (app) => {
     })
 
     app.get('/user/stats/:id', (req,res)=>{
-        console.log(req.params.id)
         db.Character.findOne({where: {UserId: req.params.id}, include: [db.Attack]}).then(stats=>{
             res.json(stats)
         })
     })
+
+    // app.get('user/:id', (req,res)=>{
+    //     db.User.findOne({where: {}})
+    // })
 }
