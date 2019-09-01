5886f9add602509af0884a784ba88f708e1a0f0b
Magic-Mayo <mike.mayoii@gmail.com>
2019-09-01 00:17:00 -0700

    added variables for luck, userid, character name, and class for form submit on character creation. created handleNewCharacter function to pass form values into newCharacter function. made modal4 open on new user creation and stored token when a new user is created. changed route for newCharacter function and redirected to new page when a character is created. changed verifyPass function to store token on successful verification and opned modal3 after creating buttons with users characters inside it. did the same for verifyToken function

--- public/js/user.js
+++ public/js/user.js
@@ -8,9 +8,12 @@ $(document).ready(()=>{
     const newWord = $('#new-word');
     const user = $('#user');
     const userPass = $('#userPass');
+    const charName = $('#char-name');
+    const luck = $('#luck');
+    let userId = $('#userId').val();
+    const newClass = $('input:checked').val();
 
-
-    const formSubmit = (e) => {
+    const handleNewUser = (e) => {
         e.preventDefault();
         
         if(!newUser.val().trim() || !newWord.val().trim()){
@@ -18,6 +21,15 @@ $(document).ready(()=>{
         }
 
         createUser({userName: newUser.val().trim(), pass: newWord.val().trim()});
+    }
+
+    const handleNewCharacter = (e) => {
+        e.preventDefault();
+        if (!luck.val().trim() || !charName.val().trim()){
+            return;
+        }
+        console.log(newClass)
+        newCharacter({id: userId, name: charName.val().trim(), luckyNum: luck.val(), class: newClass})
     }
 
     const handleVerifyPass = (e) => {
@@ -28,17 +40,22 @@ $(document).ready(()=>{
 
     const createUser = (user) => {
         $.post('/new/user', user).then(token=>{
-            localStorage.setItem('_ParkU',token)
+            localStorage.setItem('_ParkU', token);
+            $('#modal4').modal('open');
         })
     }
 
     // Future for resetting passwords
-    const userWord = (pass) => {
-        $.post('/new/word', pass)
+    const forgotWord = (user, pass) => {
+        $.post(`/new/word/${user}`, pass)
     }
 
-    const newCharacter = (newClass) => {
-        $.post('/new/char/:user', newClass)
+    const newCharacter = (newChar) => {
+        console.log(newChar)
+        $.post(`/new/char/${newChar.id}`, newChar).then(newChar=>{
+            
+            window.location.href = `/start/id=${newChar.id}`;
+        })
     }
 
     const handleDeleteUser = (e) => {
@@ -54,15 +71,34 @@ $(document).ready(()=>{
     }
 
     const verifyPass = (pass) => {
-        $.post('/word/verify', pass).then((verified)=>{
+        $.post('/word/verify', pass).then(verified=>{
             if (verified){
-                window.location.href = '/start'
+                localStorage.setItem('_ParkU', verified.token);
+                userId = verified.uid;
+                
+                if (verified.token && verified.character.length > 0){
+                    // uid = verified.
+                    console.log('verified')
+                    for (let i=0; i<verified.character.length; i++){
+                        const btn = $('<button>');
+                        btn.append(`<h4 class='char-name'>${verified.character[i].charName}</h4>`)
+                        .append(`<p class='char-stats'>Class: ${verified.character[i].class} && HP: ${verified.character[i].currentHP}`)
+                        .addClass('user-char-btn')
+                        .data('id', verified.character[i].id);
+                        $('.char-btn').append('<br>').append(btn);
+                    }
+                    $('#modal3').modal('open');
+                } else {
+                    console.log('modal 4')
+                    $('#modal4').modal('open');
+                }
             } else {
+                console.log('wrong')
                 // let user know wrong password was entered
             }
         })
     }
-
+    let uid;
     const verifyToken = (token) => {
         $.get(`/token/${token}`, (dbtoken)=>{
             console.log(dbtoken.characters)
@@ -70,23 +106,30 @@ $(document).ready(()=>{
                 for (let i=0; i<dbtoken.characters.length; i++){
                     const btn = $('<button>')
                     btn.append(`<h4 class='char-name'>${dbtoken.characters[i].charName}</h4>`)
-                    .append(`<p class='char-stats'>Class: ${dbtoken.characters[i].class} || HP: ${dbtoken.characters[i].hp}`);
-                    $('.char-btn').append(btn)
+                    .append(`<p class='char-stats'>Class: ${dbtoken.characters[i].class} && HP: ${dbtoken.characters[i].currentHP}`)
+                    .addClass('flex-btn')
+                    .data('id', dbtoken.characters[i].id);
+                    $('.char-btn').append('<br>').append(btn);
                 }
-                $('#modal3').modal('open');
+                $('#modal4').modal('open');
             } else {
                 console.log('sign in')
             }
         })
     }
 
-    if (parkUToken){
-        console.log('token here')
-        verifyToken(parkUToken);
-    }
+    // if (parkUToken){
+    //     console.log('token here')
+    //     console.log(userId.val())
+    //     verifyToken(parkUToken);
+    // }
 
-    $(document).on('submit', '#add-user', formSubmit);
-    $(document).on('click', '#delete-user', handleDeleteUser);
-    $(document).on('click', '#new-char', newCharacter);
-    $(document).on('submit', '#login', handleVerifyPass)
+    $(document).on('submit', '#add-user', handleNewUser);
+    $(document).on('submit', '#delete-user', handleDeleteUser);
+    $(document).on('submit', '#add-char', handleNewCharacter);
+    $(document).on('submit', '#login', handleVerifyPass);
+    
+    $('.char-btn').on('click', '.user-char-btn', function(){
+        window.location.href = `/start/id=${$(this).data('id')}`
+    })
 })
