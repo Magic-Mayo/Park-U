5f4a3e650ac4d05071bde6a6e5766df583110c87
Magic-Mayo <mike.mayoii@gmail.com>
2019-09-01 00:17:00 -0700

    made switch for determining hp, def, and attack table columns to be used. passed in uid when verifying password. changed route for getting character info

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -22,21 +22,42 @@ module.exports = (app) => {
 
     app.post('/new/char/:user', (req, res)=>{
         const user = req.params.user;
-        db.Character.create(req.body)
+        let def;
+        let HP;
+        let atk;
+        switch (req.body.class){
+            case 'CSS': def = 3; break;
+            case 'HTML': def = 6; break;
+            case 'Javascript': def = 5; break
+        }
+        switch (req.body.class){
+            case 'CSS': HP = 125; break;
+            case 'HTML': HP = 100; break;
+            case 'Javascript': HP = 115; break
+        }
+        switch (req.body.class){
+            case 'CSS': atk = 1; break;
+            case 'HTML': atk = 2; break;
+            case 'Javascript': atk = 3; break
+        }
+
+        db.Character.create({charName: req.body.name, class: req.body.class, defense: def, maxHP: HP, currentHP: HP, UserId: user, luck: req.body.luckyNum, AttackId: atk})
     })
 
     app.post('/word/verify', (req,res)=>{
         db.User.findOne({where: {userName: req.body.userName.toString()}, include: [db.Token]}).then(pass=>{
-            console.log(pass)
+            // console.log(pass)
             bcrypt.compare(req.body.password, pass.dataValues.pass).then((result)=>{
                 if(result){
                     console.log(pass.dataValues.id)
                     uid(18).then(newToken=>{
                         db.Token.update({token: newToken}, {where: {userId: pass.dataValues.id}});
-                        res.json(newToken)
+                        db.Character.findAll({where: {UserId: pass.dataValues.id}}).then(char=>{
+                            res.json({token: newToken, character: char, uid: pass.dataValues.id})
+                        })
                     })
                 } else {
-                    res.json('Incorrect Password')
+                    res.json(false)
                 }
             });
             
@@ -49,8 +70,9 @@ module.exports = (app) => {
         })
     })
 
-    app.get('/user/stats/:id', (req,res)=>{
-        db.Character.findOne({where: {UserId: req.params.id}, include: [db.Attack]}).then(stats=>{
+    app.get('/user/char/:id/stats', (req,res)=>{
+        console.log(req.params.id)
+        db.Character.findOne({where: {id: req.params.id}, include: [db.Attack]}).then(stats=>{
             res.json(stats)
         })
     })
