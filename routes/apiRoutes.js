6a5150b0244b6082e49ef9bc43ee82b064048ec2
Magic-Mayo <mike.mayoii@gmail.com>
2019-10-04 16:41:04 -0700

    changed callback route

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -9,7 +9,7 @@ const FacebookStrategy = require('passport-facebook').Strategy;
 passport.use(new FacebookStrategy({
     clientID: process.env.FACEBOOK_APP_ID || FACEBOOK_APP_ID,
     clientSecret: process.env.FACEBOOK_APP_SECRET || FACEBOOK_APP_SECRET,
-    callbackURL: /*'/auth/facebook/callback' ||*/ "https://park-u.herokuapp.com/auth/facebook/callback"
+    callbackURL: '/auth/facebook/callback'
     },
     function(accessToken, refreshToken, profile, done) {
         // console.log(profile)
@@ -25,6 +25,7 @@ passport.use(new FacebookStrategy({
         //     done(user);
         // }
         done(profile)
+        console.log(profile)
     // });
     }
 ));
